a07668358d897f48c69c670e5b09362e2616a76a
acbaloi <anacajingue@gmail.com>
2023-10-14 17:12:37 +0200

    Change the Name of a Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
     {name: "name3", artist: "artist3", album: "album3", id: 3},
   ],
 
-     playlistName : "Friday Jamz",
+     playlistName : "Playlist",
 
      playlistTracks : [{name: "name4", artist: "artist4", album: "album4", id: 4}]
 
@@ -22,6 +22,7 @@ class App extends React.Component {
 
  this.addTrack = this.addTrack.bind(this);
  this.removeTrack = this.removeTrack.bind(this);
+ this.updatePlaylistName = this.updatePlaylistName(this);
 }
 
  addTrack(track) {
@@ -39,7 +40,10 @@ class App extends React.Component {
   tracks = tracks.filter(currentTrack => currentTrack.id !== track.id)
 
   this.setState({playlistTracks: tracks});
+ }
 
+ updatePlaylistName(name) {
+  this.setState ({playlistName: name});
  }
 
  render() {
@@ -51,7 +55,8 @@ class App extends React.Component {
           <div className="App-playlist">
                <SearchResults searchResults={this.state.searchResults}
                onAdd={this.addTrack}/>
-               <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}/> 
+               <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack}
+               onNameChange={this.updatePlaylistName}/> 
           </div>
        </div>
       </div>
